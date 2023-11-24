5fa83565c581a3472e66d39b2b3ff9cfd21f5ada
Alexey Svistunov <xdayx53@gmail.com>
2023-11-24 10:01:40 +0300

    new ideas =>

--- script.js
+++ script.js
@@ -65,155 +65,71 @@ const cars = [
 
 ];
 
-
-const selectBrand = document.querySelector('.car__select-brand');
-const selectType = document.querySelector('.car__select-type');
-const selectSort = document.querySelector('.car__select-sort');
 const carList = document.querySelector('.car__list');
 
-function sortBy(array) {
-  if(selectSort.value === "Убывание") {
-    const newArray = array.sort((a, b) => {
-        if(a.price > b.price) {
-          return -1;
-        }
-    });
-    carList.innerHTML = "";
-    insertElements(newArray)
-    
-  } else {
-    const newArray = array.sort((a, b) => {
-      if(a.price < b.price) {
-        return -1;
-      }
-    });
-    carList.innerHTML = "";
-    insertElements(newArray)
-  }
 
+function render(cars){
+ for (let i = 0; i < cars.length; i++) {
+  carList.insertAdjacentHTML('beforeend',  `
+  <li class="car__item">
+    <img src="${cars[i].img}" alt="${cars[i].brand} ${cars[i].model}">
+    <h3 class="car__item-title">${cars[i].brand}</h3>
+    <span class="car__item-price">${cars[i].price}$</span>
+  </li>
+  `)
 }
 
 
-function render(array) {
-  insertElements(array);
-  
 }
-
-
-function getPattern(brand, model, price, img) {
-  return `<li class="car__item">
-    <img src="${img}" alt="${brand} ${model}">
-    <h3 class="car__item-title">${brand} ${model}</h3>
-    <span class="car__item-price">${price}$</span>
-  </li>`
-}
-
 
 render(cars);
 
-function sortBrand(array) {
-    if(selectBrand.value === "ALL") {
-      render(cars)
-    } else {
-      const newArray = array.filter(el => el.brand === selectBrand.value);
-       carList.innerHTML = "";
-       insertElements(newArray);
-    }
+
+function sort(){
+  const carsObj = Object.assign(cars);
+
+  // const sortedBrand = carsObj.filter(el => {
+  //   if(el.brand === "LADA") {
+  //     return el;
+  //   }
+  // });
+  
+  // carList.innerHTML = "";
+  // for (let i = 0; i < sortedBrand.length; i++) {
+  //   carList.insertAdjacentHTML('beforeend', `
+  //   <li class="car__item">
+  //     <img src="${sortedBrand[i].img}" alt="${sortedBrand[i].brand} ${sortedBrand[i].model}">
+  //     <h3 class="car__item-title">${sortedBrand[i].brand}</h3>
+  //     <span class="car__item-price">${sortedBrand[i].price}$</span>  
+  //   </li>
+  //   `)
+  // }
+
+  // const sortedOffroad = sortedBrand.filter(el => {
+  //   if(el.offroad === false) {
+  //     return el;
+  //   }
+  // });
+
+  // carList.innerHTML = "";
+  // for (let i = 0; i < sortedOffroad.length; i++) {
+  //   carList.insertAdjacentHTML('beforeend', `
+  //   <li class="car__item">
+  //     <img src="${sortedOffroad[i].img}" alt="${sortedOffroad[i].brand} ${sortedOffroad[i].model}">
+  //     <h3 class="car__item-title">${sortedOffroad[i].brand}</h3>
+  //     <span class="car__item-price">${sortedOffroad[i].price}$</span>  
+  //   </li>
+  //   `)
+  // }
+
+
+
+ // с каким объектом работать
+ // возможно функцию в функции
  
-
 }
 
-function sortType(array) {
-  if(selectType.value === "ВНЕДОРОЖНИК") {
-    const newArray = array.filter(el => el.offroad === true);
-    carList.innerHTML = "";
-    insertElements(newArray);
-  } else {
-    const newArray = array.filter(el => el.offroad === false);
-    carList.innerHTML = "";
-    insertElements(newArray);
-  }
-}
-function insertElements(array) {
-  for (let i = 0; i < array.length; i++) {
-    carList.insertAdjacentHTML('beforeend', getPattern(array[i].brand, array[i].model, array[i].price, array[i].img))   
-  }
-}
-
-
-selectBrand.addEventListener('change', function(e){
-  sortBrand(cars);
-});
-
-
-selectType.addEventListener('change', function(e){
-  sortType(cars);
-})
-
-selectSort.addEventListener('change', function(e){
-  sortBy(cars)
-})
-
-
-// Функция для отображения списка автомобилей
-// function displayCars(cars) {
-//   carList.innerHTML = ''; // Очищаем текущий список
-
-//   cars.forEach(car => {
-//     const carItem = document.createElement('li');
-//     carItem.classList.add('car__item');
-
-//     carItem.innerHTML = `
-//       <div class="car__info">
-//         <img src="${car.img}" alt="${car.brand} ${car.model}" class="car__image">
-//         <div class="car__details">
-//           <h3>${car.brand} ${car.model}</h3>
-//           <p>Цена: ${car.price.toLocaleString()} руб.</p>
-//           <p>${car.offroad ? 'Внедорожник' : 'Седан'}</p>
-//         </div>
-//       </div>
-//     `;
-
-//     carList.appendChild(carItem);
-//   });
-// }
-
-// // Функция для фильтрации и сортировки списка автомобилей
-// function filterAndSortCars() {
-//   let filteredCars = cars.slice(); // Создаем копию массива
-
-//   const selectedBrand = selectBrand.value;
-//   const selectedType = selectType.value;
-//   const selectedSort = selectSort.value;
-
-//   // Фильтрация по бренду
-//   if (selectedBrand !== 'ALL') {
-//     filteredCars = filteredCars.filter(car => car.brand === selectedBrand);
-//   }
-
-//   // Фильтрация по типу
-//   if (selectedType !== 'ALL') {
-//     filteredCars = filteredCars.filter(car => (selectedType === 'ВНЕДОРОЖНИК' ? car.offroad : !car.offroad));
-//   }
-
-//   // Сортировка
-//   if (selectedSort === 'Убывание') {
-//     filteredCars.sort((a, b) => b.price - a.price);
-//   } else if (selectedSort === 'Возрастание') {
-//     filteredCars.sort((a, b) => a.price - b.price);
-//   }
-
-//   // Отображение отфильтрованного и отсортированного списка
-//   displayCars(filteredCars);
-// }
-
-// // Обработчики событий для выпадающих списков
-// selectBrand.addEventListener('change', filterAndSortCars);
-// selectType.addEventListener('change', filterAndSortCars);
-// selectSort.addEventListener('change', filterAndSortCars);
-
-// // Инициализация списка при загрузке страницы
-// displayCars(cars);
+sort()
 
 
 
